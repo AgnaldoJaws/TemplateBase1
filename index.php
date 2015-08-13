53a1442093fea8d5459e0d9feaf80a57e6f8f9a1
Agnaldo <agnaldojr.dep.vc>
2015-08-13 16:38:21 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,328 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Layout Fixo</title>
+<link href="style.css" rel="stylesheet" />
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+
+<body>
+
+
+	<section id="home">
+
+		<div id="flip">
+
+			<h2 id="a">Menu</h2>
+
+
+		</div>
+		<div id="panel">
+			<ul>
+
+
+				<li><a href="#blog" data-scroll>Guia Turistico</a></li>
+				<li><a href="#slider" data-scroll>Galeria</a></li>
+				<li><a href="#contact" data-scroll>Contato</a></li>
+				<li><a href="#location" data-scroll>Localização</a></li>
+
+				<li><a href="#offers" data-scroll>Nossas ofertas</a></li>
+				<li><a href="#eventos" data-scroll>Reuniões e Eventos</a></li>
+				<li><a href="#slider" data-scroll>Galeria</a></li>
+				<li><a href="#home" data-scroll>Home</a></li>
+			</ul>
+		</div>
+		<!-- Final Menu -->
+
+
+
+
+		<div class="container">
+			<header class="header"> </header>
+
+			<div class="content">
+				<div class="content-main">
+
+					<article class="hero">
+						<h2>Procurando um lugar legal para ficar por alguns dias ?</h2>
+						<br />
+
+						<p class="brief">Descrição ou resumo de um conteúdo importante,
+							que precisa ser mostrado para evidenciar a notoriedade deste
+							conteúdo. Descrição ou resumo de um conteúdo importante, que
+							precisa ser mostrado para evidenciar a notoriedade deste
+							conteúdo.</p>
+
+					</article>
+
+				</div>
+
+
+			</div>
+
+
+		</div>
+
+
+	</section>
+
+	<article id="about"></article>
+
+	<article id="eventos">
+		<div id="eventContent">
+
+			<h2>Tipos de evento.</h2>
+			<br /> <img src="img/img6.jpg">
+
+			<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria
+				tipográfica e de impressos, e vem sendo utilizado desde o século XVI</p>
+			<br>
+			<ul>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li><a href="#home" data-scroll>Topo</a></li>
+				<br />
+
+
+			</ul>
+		</div>
+		<div id="eventContent">
+
+			<h2>Serviços oferecidos.</h2>
+			<br /> <img src="img/img6.jpg">
+
+			<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria
+				tipográfica e de impressos, e vem sendo utilizado desde o século XVI</p>
+			<br>
+			<ul>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li><a href="#home" data-scroll>Topo</a></li>
+				<br />
+
+
+			</ul>
+
+
+		</div>
+		<div id="eventContent">
+
+			<h2>Mapa do local.</h2>
+			<br /> <img src="img/img6.jpg">
+
+			<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria
+				tipográfica e de impressos, e vem sendo utilizado desde o século XVI</p>
+			<br>
+			<ul>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li>tipográfica e de impressos</li>
+				<li><a href="#home" data-scroll>Topo</a></li>
+				<br />
+
+			</ul>
+
+
+		</div>
+
+	</article>
+	<!-- final article evento -->
+
+
+	<article id="offers">
+		<h1>Promoções e Ofertas</h1>
+
+		<table class="table" border="1">
+
+			<thead>
+				<tr>
+					<th>Suite</th>
+					<th>Quantidade</th>
+					<th>Valor</th>
+					<th>Descrição</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar,com tv, ar condicionado e
+						frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+			</tbody>
+		</table>
+		<br>
+		<h1>Promoções e Ofertas</h1>
+		<table class="table" border="1">
+
+			<thead>
+				<tr>
+					<th>Suite</th>
+					<th>Quantidade</th>
+					<th>Valor</th>
+					<th>Descrição</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar,com tv, ar condicionado e
+						frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+			</tbody>
+		</table>
+		<br>
+		<h1>Promoções e Ofertas</h1>
+
+		<table class="table" border="1">
+
+			<thead>
+				<tr>
+					<th>Suite</th>
+					<th>Quantidade</th>
+					<th>Valor</th>
+					<th>Descrição</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar,com tv, ar condicionado e
+						frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+				<tr>
+					<td>Standard</td>
+					<td>1 Pessoa</td>
+					<td>R$ 140,00 Dia</td>
+					<td>com tv, ar condicionado e frigobar</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<a href="#home" data-scroll>Topo</a>
+
+	</article>
+	<!-- final article offers -->
+
+	<article id="location">
+		<h2>Localização</h2>
+		<iframe
+			src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d462720.80344247166!2d-47.98842770000002!3d-25.033861049999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dae4bd9223c3bb%3A0x4f29622104147532!2zQ2FuYW7DqWlhLCBTUA!5e0!3m2!1spt-BR!2sbr!4v1439488271395"
+			frameborder="0" style="border: 0" allowfullscreen></iframe>
+
+	</article>
+
+	<article id="contact">
+		<h2>Contato</h2>
+
+
+	</article>
+
+	<article id="slider">
+		<div id='carousel_container'>
+
+			<div id='carousel_inner'>
+				<ul id='carousel_ul'>
+					<li><a href='#'><img src='img/img6.jpg' /></a></li>
+					<li><a href='#'><img src='img/img7.jpg' /></a></li>
+					<li><a href='#'><img src='img/img8.png' /></a></li>
+
+				</ul>
+
+			</div>
+		</div>
+		</div>
+
+	</article>
+
+
+	<script> 
+$(document).ready(function(){
+    $("#a").click(function(){
+        $("#panel").slideToggle("slow");
+    });
+});
+</script>
+
+</body>
+</html>
